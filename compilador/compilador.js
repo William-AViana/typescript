e8e455a8e148b90bb765620af287ef66d0dcd775
William Viana <willdev.viana@gmail.com>
2022-06-29 22:35:34 -0300

    create and manipulation file with onEmitOnError

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -1,1 +1,12 @@
 "use strict";
+let canal = 'Gaveta';
+let inscritos = 6524;
+// canal = inscritos
+console.log(`Canal = ${canal}!`);
+// let nome: string = 'Ana'
+/* A variavél "nome" já foi declarada em tipos.ts, nesse cenário
+   não tem o uso de módulos para poder ter namespaces diferentes,
+   para poder usar nomes de formas repetidas.
+*/
+nome = 'Pedro';
+console.log(`Nome = ${nome}`);
---
+++ compilador/compilador.ts
@@ -0,0 +1,13 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 6524
+
+// canal = inscritos
+console.log(`Canal = ${canal}!`)
+
+// let nome: string = 'Ana'
+/* A variavél "nome" já foi declarada em tipos.ts, nesse cenário
+   não tem o uso de módulos para poder ter namespaces diferentes,
+   para poder usar nomes de formas repetidas.
+*/
+nome = 'Pedro'
+console.log(`Nome = ${nome}`)
