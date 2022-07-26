64c06336cc5deb25a1f09f51074b774b676cdb63
William Viana <willdev.viana@gmail.com>
2022-07-25 22:24:20 -0300

    undrestanding rest & spread with tuple

--- ecmascript/ecmascript.js
+++ ecmascript/ecmascript.js
@@ -91,4 +91,15 @@ function retornarArray(...args) {
 }
 const numeros = retornarArray(1, 2, 4, 5, 6, 7, 8);
 console.log(numeros);
+// Rest & Spread (tupla)
+const tupla = [1, 'Teste', false];
+function tuplaParam1(a, b, c) {
+    console.log(`1) ${a} ${b} ${c}`);
+}
+tuplaParam1(...tupla);
+function tuplaParam2(...params) {
+    console.log(Array.isArray(params));
+    console.log(`2) ${params[0]} ${params[1]} ${params[2]}`);
+}
+tuplaParam2(...tupla);
 //# sourceMappingURL=ecmascript.js.map
--- ecmascript/ecmascript.ts
+++ ecmascript/ecmascript.ts
@@ -115,3 +115,18 @@ function retornarArray(...args: number[]): number[] {
 const numeros = retornarArray(1, 2, 4, 5, 6, 7, 8)
 console.log(numeros)
 
+// Rest & Spread (tupla)
+const tupla: [number, string, boolean] = [1, 'Teste', false]
+
+function tuplaParam1(a: number, b: string, c: boolean): void {
+  console.log(`1) ${a} ${b} ${c}`)
+}
+
+tuplaParam1(...tupla)
+
+function tuplaParam2(...params: [number, string, boolean]): void {
+  console.log(Array.isArray(params))
+  console.log(`2) ${params[0]} ${params[1]} ${params[2]}`)
+}
+
+tuplaParam2(...tupla)
